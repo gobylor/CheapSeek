7f0f65426b1331cce8d9e9a9597967860539a5c4
Lor <1754009643@QQ.COM>
2025-02-14 10:47:15 +0800

    feat: deep think

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "math/rand"
+    "os"
+    "strings"
+    "time"
+)
+
+func simulateThinking() {
+    thinkTime := rand.Intn(5) + 1
+    fmt.Print("thinking")
+    for i := 0; i < thinkTime; i++ {
+        fmt.Printf("...%ds", i)
+        time.Sleep(time.Second)
+        fmt.Print("\b\b\b\b\b") 
+    }
+    fmt.Print("\r" + strings.Repeat(" ", 20) + "\r") 
+}
 
 func main() {
-    fmt.Println("The server is busy. Please try again later.\n服务器繁忙，请稍后再试。\n")
+    reader := bufio.NewReader(os.Stdin)
+    
+    fmt.Println("Startup successful! Ready to process requests.")
+    fmt.Println("Welcome to NoGPUDeepSeek!")
+    fmt.Println("Enter your prompt (or 'quit' to exit):")
+
+    for {
+        fmt.Print("\n>>> ")
+        input, _ := reader.ReadString('\n')
+        input = strings.TrimSpace(input)
+
+        if input == "quit" {
+            fmt.Println("Goodbye!")
+            break
+        }
+
+        if input == "" {
+            continue
+        }
+
+        simulateThinking()
+        
+        fmt.Println("The server is busy. Please try again later.\n服务器繁忙，请稍后再试。\n")
+    }
 }
